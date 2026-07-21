86a8fbb06d880e9d00c55a1b6372ce7bf17a1d41
Lucas Correa <lucas.dorneles.correa96@gmail.com>
2026-07-21 00:06:49 +0000

    Scaled the reading and writing process to load all the datasets at once.

--- src/ingestion/raw_to_bronze.py
+++ src/ingestion/raw_to_bronze.py
@@ -1,15 +1,31 @@
 # raw_to_bronze.py
 from pyspark.sql import functions as F
+from pathlib import Path
 
-def raw_to_bronze(spark, source_path, destination_path):
+
+def raw_to_bronze(spark, source_path, destination_folder):
     """read csv files from raw and move it to bronze layer"""
-    df = (
-        
-        spark.read
-            .options(header=True, inferSchema=True)
-            .csv(source_path)
-            .withColumn("ingestion_timestamp", F.current_timestamp())
-            .withColumn("source_file", F.input_file_name())
-    )
-    df = df.coalesce(2)
-    df.write.parquet(destination_path, mode="overwrite")
+
+    files = Path(source_path).glob("*.csv")
+
+    if not files:
+        raise FileNotFoundError(f"No CSV files found in {source_path}")
+
+
+    for file in files:
+
+        dataset_name = file.stem
+        print(f"Processing {dataset_name}...")
+
+        df = (
+            
+            spark.read
+                .options(header=True, inferSchema=True)
+                .csv(str(file))
+                .withColumn("ingestion_timestamp", F.current_timestamp())
+                .withColumn("source_file", F.lit(file.name))
+        )
+
+
+
+        df.write.parquet(f"{destination_folder}/{dataset_name}", mode="overwrite")
